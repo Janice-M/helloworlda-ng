2bc218d20df281721f371afc514dd29bbca8a3ec
Janice-M <janiceink001@gmail.com>
2019-07-10 19:01:43 +0300

    interpolation in Angular

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `<div>
-  npm
+Welcome {{name}}
   </div>`,
   styles: [` div{
   color: red} `]
 })
 export class TestComponent implements OnInit {
-
+ public name = "Janice";
   constructor() { }
 
   ngOnInit() {

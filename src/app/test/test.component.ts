93ee0fdaf32f2b689b2aa7af505caeec43565396
Janice-M <janiceink001@gmail.com>
2019-07-11 11:10:02 +0300

    hellow

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,27 +2,22 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: `<div>
-Welcome {{name}}
-  </div>
-  <h2>{{2+2}}</h2>
-  <h2>{{"welcome " + name }}</h2>
-  <h2>{{ name.length }}</h2>
-  <h2>{{ name.toUpperCase() }}</h2>
-  <h2>{{ greetUser() }}</h2>
+  template: `
+  <h2>
+  Welcome {{name}}
+  </h2>
+  <input type="text" value="Janice">
   `
   ,
   styles: [` div{
   color: red} `]
 })
 export class TestComponent implements OnInit {
- public name = "Janice";
+  public name = "Janice";
   constructor() { }
 
   ngOnInit() {
   }
-  greetUser(){
-    return "Hello " +this.name;
-  }
+
 }
 
